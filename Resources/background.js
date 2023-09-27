af10fc261ed2aa09918b274404dcf3ecce071a0d
Tanuj <Tanuj@cmu.edu>
2023-09-27 04:52:16 -0700

    remove unnecessary season selection

--- Resources/background.js
+++ Resources/background.js
@@ -1,28 +1,18 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-    const lastWatchedEpisode = localStorage.getItem("lastWatchedEpisode");
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
-//        sendResponse({ farewell: "goodbye" });
-});
-function
-// Retrieve the last watched episode from storage
-const lastWatchedEpisode = localStorage.getItem("lastWatchedEpisode");
+// browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   console.log("Received request: ", request);
+//   let episode;
+//   // const lastWatchedEpisode = Fetch current episode here from local storage or somehow
+//   safari.extension.settings.lastWatchedEpisode = "Episode 1";
+//   const lastWatchedEpisode = safari.extension.settings.lastWatchedEpisode;
+//   if (lastWatchedEpisode) {
+//       episode = lastWatchedEpisode;
+//   }
+//   else {
+//       episode = "Episode 1";
+//   }
+//   // send episode
+//   if (request.greeting === "hello")
+//       sendResponse({ farewell: episode });
+// //        sendResponse({ farewell: "goodbye" });
+// });
 
-// Perform action based on the value of lastWatchedEpisode
-if (lastWatchedEpisode === "Episode 5") {
-  // Run a specific function for Episode 5
-  functionForEpisode5();
-} else if (lastWatchedEpisode === "Episode 6") {
-  // Run a specific function for Episode 6
-  functionForEpisode6();
-}
-
-safari.extension.addEventListener("message", function (event) {
-  if (event.name === "episodeWatched") {
-    // Handle the message and store the last watched episode
-    const lastWatchedEpisode = event.message.episode;
-    // Store the value using localStorage or any other desired storage mechanism
-    localStorage.setItem("lastWatchedEpisode", lastWatchedEpisode);
-  }
-});
--- Resources/content.js
+++ Resources/content.js
@@ -1,57 +1,69 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-   console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-   console.log("Received request: ", request);
-});
-
-
 // The season to select
 const selectedSeason = 7;
 
+function checkSeason() {
+ // returns season like -> "Season 7"
+ const button = document.querySelector('.filter.js-filter');
+ if (!button) {
+   console.error('Season button not found');
+   return null;
+ }
+ try {
+   const buttonSeason = button.textContent.trim(); // Get the text content of the button and remove any leading/trailing whitespace
+   return buttonSeason;
+ }
+ catch (e) {
+   console.log('buttonSeason text content not found');
+   return null;
+ }
+}
 // Function to select the season in the dropdown
 function selectSeason() {
-  const dropdown = document.getElementById('season_filterDD');
-  const seasonOption = dropdown.querySelector(`[data-value="${selectedSeason}"]`);
-
-  if (seasonOption) {
-    seasonOption.click();
-  } else {
-    console.error('Season option not found');
-  }
+ const dropdown = document.getElementById('season_filterDD');
+ // if default season does not match desired season, select season option
+ if (checkSeason() === `Season ${selectedSeason}`) {
+     console.log("correct season already selected.");
+   return;
+ }
+console.log("selecting season now.");
+ const seasonOption = dropdown.querySelector(`[data-value="${selectedSeason}"]`);
+ if (seasonOption) {
+   seasonOption.click();
+ } else {
+   console.log('Season option not found');
+ }
 }
 
 // Function to trigger "Show More" button click after a random delay
 function triggerShowMore() {
-  const showMoreButton = document.querySelector('.button.secondary.focusable.load-more-button.js-load-more');
+ const showMoreButton = document.querySelector('.button.secondary.focusable.load-more-button.js-load-more');
 
-  if (showMoreButton) {
-    showMoreButton.click();
-  } else {
-    console.error('Show More button not found');
-  }
+ if (showMoreButton) {
+   showMoreButton.click();
+ } else {
+   console.error('Show More button not found');
+ }
 }
 
 // Generate a random delay between min and max (inclusive)
 function getRandomDelay(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+ return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+ return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  await selectSeason();
-  const delayTime = getRandomDelay(500, 1000);
-  await delay(delayTime);
-  triggerShowMore();
+ await selectSeason();
+ const delayTime = getRandomDelay(500, 1000);
+ await delay(delayTime);
+ triggerShowMore();
 }
 
 if (!window.location.href.includes('https://www.paramountplus.com/shows/survivor/')) {
-    console.log("No need to run any scripts here :)");
+   console.log("No need to run any scripts here :)");
 }
 else {
-    console.log('Extension active on paramount plus!');
-    main();
+   console.log('Extension active on paramount plus!');
+   main();
 }
